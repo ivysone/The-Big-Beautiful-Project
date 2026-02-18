6051fc75e74b2490b405f29704aabd1401e9712c
DarsshanP <darsshanpalaram@gmail.com>
2026-02-18 20:09:33 +0000

    Bug fix on heart pickups

--- static/src/entities/pickups/HeartPickups.js
+++ static/src/entities/pickups/HeartPickups.js
@@ -3,37 +3,43 @@ export default class HeartPickup {
     this.scene = scene;
     this.healAmount = healAmount;
 
-    // Use a Matter sprite so we can play an animation
+    // Use a Matter sprite because you want animation frames
     this.sprite = scene.matter.add.sprite(x, y, "heart_pickup", 0, {
       isSensor: true,
-      isStatic: true,
-      ignoreGravity: true,
+      isStatic: false,
+      ignoreGravity: false, // set true if you want it to float
     });
 
     this.sprite.setDepth(10);
     this.sprite.setScale(1);
     this.sprite.setFrictionAir(0.05);
+    this.sprite.setFixedRotation();
 
-    // Tag for collision detection
+    // tags for collision detection
     this.sprite.isHeartPickup = true;
     this.sprite.heartPickupRef = this;
 
-    // Play the animation (make sure it's created in the scene)
-    if (scene.anims.exists("heart_idle")) {
-      this.sprite.play("heart_idle");
-    }
+    // animation
+    this.sprite.play("heart_idle");
 
-    // Tween the heart sprite (not the scene)
-    scene.tweens.add({
+    // IMPORTANT: tween the sprite, not the scene
+    this.floatTween = scene.tweens.add({
       targets: this.sprite,
       y: y - 6,
       duration: 180,
       yoyo: true,
-      ease: "Sine.easeOut",
+      repeat: -1,
+      ease: "Sine.easeInOut",
     });
   }
 
   destroy() {
+    if (this.floatTween) {
+      this.floatTween.stop();
+      this.scene.tweens.remove(this.floatTween);
+      this.floatTween = null;
+    }
+
     if (this.sprite) {
       this.sprite.destroy();
       this.sprite = null;
