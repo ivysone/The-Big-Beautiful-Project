7a56e953f638c33658068bc5fe15b2aee57f87b7
DarsshanP <darsshanpalaram@gmail.com>
2026-03-25 16:28:36 +0000

    level nine

---
+++ static/src/scenes/Level9/LevelNine.js
@@ -0,0 +1,8 @@
+import { BaseLevel } from "../BaseLevel.js";
+import { levelNineConfig } from "./LevelNineConfig.js";
+
+export class levelNine extends BaseLevel {
+  constructor() {
+    super(levelNineConfig);
+  }
+}
